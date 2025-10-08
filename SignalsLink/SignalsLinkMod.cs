73db2593e6dd53737516b995c7c6c347e0a4f763
Filip Nowak <fipil@spincoders.com>
2025-10-08 20:21:13 +0200

    Add project files.

---
+++ SignalsLink/SignalsLinkMod.cs
@@ -0,0 +1,43 @@
+﻿
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+
+[assembly: ModInfo("Signals Link", "signalslink",
+    Description = "Extends Signals mod with sensors and control elements for interacting with other mods and vanilla blocks.",
+    Website = "",
+    Version = "0.0.1",
+    Authors = new[] { "fipil" })]
+
+namespace signalslink.src
+{
+    public class SignalsLinkMod : ModSystem
+    {
+        static string MODID = "signalslink";
+        ICoreAPI api;
+
+        IServerNetworkChannel serverChannel;
+        IClientNetworkChannel clientChannel;
+
+        public override void Start(ICoreAPI api)
+        {
+            this.api = api;
+            base.Start(api);
+
+
+
+        }
+
+        public override void StartClientSide(ICoreClientAPI api)
+        {
+            api.World.Logger.EntryAdded += OnClientLogEntry;
+        }
+
+        private void OnClientLogEntry(EnumLogType logType, string message, params object[] args)
+        {
+            if (logType == EnumLogType.VerboseDebug) return;
+            System.Diagnostics.Debug.WriteLine("[Client " + logType + "] " + message, args);
+        }
+    }
+}
+
